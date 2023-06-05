7874372e0478a9b2d9ea44270c674fcd188ac37b
py0859@gmail.com <py0859@gmail.com>
2023-06-05 09:50:00 -0700

    deploy my personnal portfolio

---
+++ precache-manifest.d5a935cefb354ca19785b0e33047d920.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "07a3135bd2d08cb16079",
+    "url": "/Portfolio_Me/static/css/main.25669695.chunk.css"
+  },
+  {
+    "revision": "07a3135bd2d08cb16079",
+    "url": "/Portfolio_Me/static/js/main.07a3135b.chunk.js"
+  },
+  {
+    "revision": "3239492d0acf91b27198",
+    "url": "/Portfolio_Me/static/css/1.ea582623.chunk.css"
+  },
+  {
+    "revision": "3239492d0acf91b27198",
+    "url": "/Portfolio_Me/static/js/1.3239492d.chunk.js"
+  },
+  {
+    "revision": "a48e33e7ac739502d794",
+    "url": "/Portfolio_Me/static/js/runtime~main.a48e33e7.js"
+  },
+  {
+    "revision": "00462be46fed03acb7dbb46a227b830b",
+    "url": "/Portfolio_Me/static/media/home_page.00462be4.png"
+  },
+  {
+    "revision": "a5672b9a6fe4b6edf1784478807fb2ab",
+    "url": "/Portfolio_Me/static/media/me.a5672b9a.jpg"
+  },
+  {
+    "revision": "da592ec8f843dd886ea86e6ea0c546a7",
+    "url": "/Portfolio_Me/static/media/Portfolio.da592ec8.png"
+  },
+  {
+    "revision": "74a835c467436ae32ba40baea131b845",
+    "url": "/Portfolio_Me/static/media/boutik.74a835c4.jpg"
+  },
+  {
+    "revision": "e3a612349b612984c2d7ca8e04c004ed",
+    "url": "/Portfolio_Me/index.html"
+  }
+];
